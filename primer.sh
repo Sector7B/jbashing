f0bfbe8a6539f00f05086e76b3d18e4d1fff2e6e
Joseph Greenawalt <joegreenawalt@startmail.com>
2015-12-08 00:49:22 -0500

    moved jbupdate to function

--- primer.sh
+++ primer.sh
@@ -16,8 +16,9 @@ done
 #MANAGEMENT
 function jbenable { ln -s $JBAVAILABLE_FOLDER/$1.sh $JBENABLED_FOLDER/$1.sh; }
 function jbdisable { rm $JBENABLED_FOLDER/$1.sh; }
+function jbupdate { JBCD=`pwd`; cd $JBAPPROOT && git pull && cd $JBCD }
 #function jbinstall { ln -s $1 $JBENABLED_FOLDER/$1; }
 
-export -f jbenable jbdisable #jbinstall
+export -f jbenable jbdisable jbupdate #jbinstall
 
-alias jbupdate='cd $JBAPPROOT && git pull'
+#alias jbupdate='cd $JBAPPROOT && git pull'
