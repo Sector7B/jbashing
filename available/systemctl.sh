c7ceb527d9f9dfcc372a0c6ef6ba1b2f9be52751
Sector7B <joe.greenawalt@gmail.com>
2015-12-20 15:01:24 -0500

    update systemctl

--- available/systemctl.sh
+++ available/systemctl.sh
@@ -1,3 +1,4 @@
-alias sc-enable='systemctl enable $1'
-alias sc-disable='systemctl disable $1'
+alias sc-enable='sudo systemctl enable $1'
+alias sc-start='sudo systemctl start $1'
+alias sc-disable='sudo systemctl disable $1'
 alias sc-status='systemctl status $1 -l'
