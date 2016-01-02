06bcf4b6bfc1d572707e5beefce20dbb6f27d95e
Joseph Greenawalt <jgreenaw@localhost.localdomain>
2016-01-01 20:08:51 -0500

    pulled redhat and debian specific out of universal

---
+++ available/debian.sh
@@ -0,0 +1,6 @@
+alias aptc='sudo apt-get autoclean; sudo apt-get clean; sudo apt-get autoremove'
+alias aptf='aptitude show $1'
+alias apts='apt-cache search $1'
+alias apti='sudo apt-get install -y $1'
+alias aptu='sudo apt-get update; sudo apt-get -y upgrade'
+alias aptud='sudo apt-get update; sudo apt-get -y dist-upgrade'
---
+++ available/redhat.sh
@@ -0,0 +1,12 @@
+#fedora / redhat
+alias dnfc='sudo dnf -y clean all'
+alias dnfu='sudo dnf -y update'
+alias dnfi='sudo dnf -y install $1'
+alias dnfs='dnf search $1'
+alias dnff='dnf info $1'
+
+alias yumc='sudo yum -y clean all'
+alias yumu='sudo yum -y update'
+alias yumi='sudo yum -y install'
+alias yums='yum search'
+alias yumf='yum info'
